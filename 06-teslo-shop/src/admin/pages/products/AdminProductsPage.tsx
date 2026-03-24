9d7235d9de60262d2704c4174cbaaad2516704f5
Kevin <kevin.lema02@epn.edu.ec>
2026-03-24 16:35:55 -0500

    feat: Implement registration page with form validation and social login options
    feat: Update button component to include cursor pointer style
    
    feat: Enhance footer component with custom logo
    
    feat: Refactor header component to include custom logo and search functionality
    
    feat: Add placeholder SVG for image loading
    
    feat: Create activity feed component for admin dashboard
    
    feat: Develop admin header with search and notification icons
    
    feat: Implement admin sidebar with navigation links and user profile
    
    feat: Add admin title component for page headers
    
    feat: Create chart component for data visualization
    
    feat: Develop quick actions component for admin dashboard
    
    feat: Implement stat card component for displaying key metrics
    
    feat: Create custom logo component for branding
    
    feat: Add table component with reusable table structure

--- 06-teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
+++ 06-teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import { AdminTitle } from "@/admin/components/AdminTitle";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { PlusIcon } from "lucide-react";
+import { Link } from "react-router";
 
 export const AdminProductsPage = () => {
-    return (
-        <div>AdminProductsPage</div>
-    )
-}
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <AdminTitle
+          title="Productos"
+          subtitle="Aqui puedes ver y administrar tus productos"
+        />
+
+        <div className="flex justify-end mb-10 gap-4">
+          <Link to={"/admin/products/new"}>
+            <Button>
+              <PlusIcon />
+              Nuevo Producto
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+      <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10 ">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Imagen </TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Precio</TableHead>
+            <TableHead>Categoria</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead>Tallas</TableHead>
+            <TableHead className="text-right">Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>
+              <img
+                src="https://placehold.co/250x250"
+                alt="Product"
+                className="w-20 h-20 object-cover rounded-md"
+              />
+            </TableCell>
+            <TableCell>Producto 1</TableCell>
+            <TableCell>$99.99</TableCell>
+            <TableCell>Categoria 1 </TableCell>
+            <TableCell>100 Stock</TableCell>
+            <TableCell>XS, S , L</TableCell>
+            <TableCell className="text-right">
+              <Link to={`/admin/products/t-shirt-teslo'}`}>Editar</Link>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <CustomPagination totalPages={10} />
+    </>
+  );
+};
